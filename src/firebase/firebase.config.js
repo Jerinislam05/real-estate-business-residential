5b56a5b2ba8c95eb24f0cad8f10066a0513d9b9e
Jerinislam05 <jerinislam05@gmail.com>
2024-10-09 18:33:20 +0100

    project setup and google font added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLOyfya6ZWJ3FmD2udTTYYg7-gSz_Kz5c",
+  authDomain: "real-estate-residential-7af62.firebaseapp.com",
+  projectId: "real-estate-residential-7af62",
+  storageBucket: "real-estate-residential-7af62.appspot.com",
+  messagingSenderId: "693800909343",
+  appId: "1:693800909343:web:74fd99e876e2be8f20332a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-serif">This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
